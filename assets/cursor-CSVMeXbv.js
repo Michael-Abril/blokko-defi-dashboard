0f0f688e1f13478f6dcfc163ad0a0abb169def39
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 17:55:56 -0400

    Updates

--- assets/cursor-CSVMeXbv.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-CSfWIgJa.js";import"./index-CmLGhw_K.js";import"./events-ChXavjEe.js";import"./index.es-BJAWNlNY.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
