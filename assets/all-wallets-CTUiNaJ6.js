6b5de6c3b2cac4afcd81b8171a0b3c4b4fb7a016
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 16:36:30 -0400

    Updates

--- assets/all-wallets-CTUiNaJ6.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-hK8FUBwe.js";import"./index-DYcQgrwy.js";import"./events-oItSHdC7.js";import"./index.es-CKOUf2_d.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
