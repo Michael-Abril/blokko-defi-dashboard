991a582814ecdf8187a42cfedec2ed108ba2309d
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 16:39:32 -0400

    Updates

---
+++ assets/all-wallets-B8w6LDdA.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-bcFe0iWr.js";import"./index-CporTN7N.js";import"./events-DomuUWOd.js";import"./index.es-HTCLSD4C.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
