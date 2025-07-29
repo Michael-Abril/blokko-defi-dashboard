9c29f32701ea91377097a9678ff83645c57d5ec9
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 16:30:47 -0400

    Updates

---
+++ assets/chevron-top-DWGO25gQ.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-t60FeQib.js";import"./index-BZ3iadF2.js";import"./events-K7nMmFHK.js";import"./index.es-7LyLRqGk.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronTopSvg};
