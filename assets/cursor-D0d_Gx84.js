a86b6cdab92031b314dcf2a803eca87b6b7ac72c
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 19:03:38 -0400

    Updates

---
+++ assets/cursor-D0d_Gx84.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-B3f8T8vu.js";import"./index-CghE2Trh.js";import"./events-CAVRk9m9.js";import"./index.es-0HHoMnVb.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
