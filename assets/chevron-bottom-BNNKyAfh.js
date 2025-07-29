5503fa4aeac3ecf24a755ae5e9a8c54ac838e61d
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 16:48:49 -0400

    Updates

--- assets/chevron-bottom-BNNKyAfh.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-bcFe0iWr.js";import"./index-CporTN7N.js";import"./events-DomuUWOd.js";import"./index.es-HTCLSD4C.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronBottomSvg};
