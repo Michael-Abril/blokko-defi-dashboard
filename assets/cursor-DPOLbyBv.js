2cead7ad4fcb33a09f49d24f08c5bf27d86c5879
Michael-Abril <107504883+Michael-Abril@users.noreply.github.com>
2025-07-29 17:47:45 -0400

    Updates

---
+++ assets/cursor-DPOLbyBv.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-D5fLaBMc.js";import"./index-BIDc0MvX.js";import"./events-CD31naFn.js";import"./index.es-B7x2oa-y.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
